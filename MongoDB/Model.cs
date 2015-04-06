39a7a5a4b0b34a37c7287185467fcc6e012e15af
StephRagazzi <stephane.ragazzi@gmail.com>
2015-04-06 20:43:16 +0200

    Tests with MongoDB

--- MongoDB/Model.cs
+++ MongoDB/Model.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace MongoDBApp
 {
-    public class Model
+    public class Model : Entity
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        //public XmlDocument config { get; set; }
+        public string config { get; set; }
+        public ObjectId xmlFileRef { get; set; }
     }
 }
--- MongoDB/Program.cs
+++ MongoDB/Program.cs
@@ -1,24 +1,89 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 using MongoDBApp.Properties;
+using MongoRepository;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace MongoDBApp
 {
     class Program
     {
+        static MongoRepository<Model> repository = new MongoRepository<Model>();
+
         static void Main(string[] args)
+        {
+            //ConnectFromSettings();
+            //ConnectFromAppConfig();
+            ConnectFromSettingsGridFs();
+
+        }
+
+        static void ConnectFromSettings()
         {
             var client = new MongoClient(Settings.Default.AppConnectionString);
             var server = client.GetServer();
             var database = server.GetDatabase(Settings.Default.DatabaseName);
-            var t=database.GetStats();
+            var t = database.GetStats();
+
             var model = new Model() { FirstName = "Jane", LastName = "Doe" };
             var collection = database.GetCollection<Model>("model");
-            var write=collection.Insert(model);
+            var write = collection.Insert(model);
+
+        }
+
+        static void ConnectFromAppConfig()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><root><product>Product</product></root>");
+
+            var john = new Model() { FirstName = "John", LastName = "Doe", config = xmlDoc.InnerXml };
+            var jane = new Model() { FirstName = "Jane", LastName = "Doe", config = xmlDoc.InnerXml };
+            var jerry = new Model() { FirstName = "Jerry", LastName = "Maguire", config = xmlDoc.InnerXml };
+            repository.Add(new[] { john, jane, jerry });
+
+            //Show contents of DB
+            //DumpData();
+
+        }
+
+        static void ConnectFromSettingsGridFs()
+        {
+            var client = new MongoClient(Settings.Default.AppConnectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase(Settings.Default.DatabaseName);
+
+            var fileName = "test1.xml";
+            var newFileName = "testRES.xml";
+            using (var fs = new FileStream(fileName, FileMode.Open))
+            {
+                var gridFsInfo = database.GridFS.Upload(fs, fileName);
+                var fileId = gridFsInfo.Id.AsObjectId;
+
+                var model = new Model() { FirstName = "Jane", LastName = "Doe", xmlFileRef = fileId };
+                var collection = database.GetCollection<Model>("model");
+                var write = collection.Insert(model);
+
+                var file = database.GridFS.FindOne(Query.EQ("_id", fileId));
+
+                using (var stream = file.OpenRead())
+                {
+                    var bytes = new byte[stream.Length];
+                    stream.Read(bytes, 0, (int)stream.Length);
+                    using (var newFs = new FileStream(newFileName, FileMode.Create))
+                    {
+                        newFs.Write(bytes, 0, bytes.Length);
+                    }
+                }
+
+            }
+
         }
     }
 }
